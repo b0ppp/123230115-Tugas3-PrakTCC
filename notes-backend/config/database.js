3bfb0d2c4a62886fd67da36b03bef010d8bb3964
b0ppp <kalladararaja84@gmail.com>
2026-05-11 10:34:47 +0700

    update database.js

--- notes-backend/config/database.js
+++ notes-backend/config/database.js
@@ -2,11 +2,15 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 const db = mysql.createPool({
   host:     process.env.DB_HOST     || '34.172.113.167',
   user:     process.env.DB_USER     || 'admin',
-  password: process.env.DB_PASSWORD || '#password#',
-  database: process.env.DB_NAME     || 'notes_NIM',
+  password: process.env.DB_PASSWORD || 'mypassword',
+  database: process.env.DB_NAME     || 'notes_123230115',
   waitForConnections: true,
   connectionLimit: 10,
 });
